98bb136d4b462fd588e39f33c76dd28a44791e57
TJ Holowaychuk <tj@vision-media.ca>
2012-11-12 14:39:40 -0800

    example

--- example.js
+++ example.js
@@ -6,4 +6,17 @@ var auth = fs.readFileSync('.auth', 'ascii').trim().split(':');
 var cloud = new Cloud('domify', auth[0], auth[1]);
 cloud.browser('iphone', '5.0', 'Mac 10.6');
 cloud.url('http://localhost:3000/test/');
+
+cloud.on('init', function(browser){
+  console.log('  init : %s %s', browser.browserName, browser.version);
+});
+
+cloud.on('start', function(browser){
+  console.log('  start : %s %s', browser.browserName, browser.version);
+});
+
+cloud.on('end', function(browser, res){
+  console.log('  end : %s %s : %d failures', browser.browserName, browser.version, res.failures);
+});
+
 cloud.start();
