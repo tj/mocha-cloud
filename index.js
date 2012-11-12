67762a29f0c072f58163a4af39e7f70c2b184429
TJ Holowaychuk <tj@vision-media.ca>
2012-11-12 14:10:49 -0800

    add .browser()

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 
+/**
+ * Module dependencies.
+ */
+
+var Emitter = require('events').EventEmitter;
+
+/**
+ * Expose `Cloud`.
+ */
+
+module.exports = Cloud;
+
+/**
+ * Initialize a cloud test with your saucelabs username / key.
+ *
+ * @param {String} user
+ * @param {String} key
+ * @api public
+ */
+
+function Cloud(user, key) {
+  this.user = user;
+  this.key = key;
+  this.browsers = [];
+  // this.browser = wd.remote('ondemand.saucelabs.com', 80, user, key);
+}
+
+/**
+ * Add browser for testing.
+ *
+ * View https://saucelabs.com/docs/browsers for details.
+ *
+ * @param {String} name
+ * @param {String} version
+ * @param {String} platform
+ * @return {Cloud} self
+ * @api public
+ */
+
+Cloud.prototype.browser = function(name, version, platform){
+  this.browsers.push({
+    browserName: name,
+    version: version,
+    platform: platform
+  });
+};
